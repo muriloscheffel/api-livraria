75d226ae39854153124acf185b51a5df8498e0f7
Murilo Scheffel <murilo.scheffel@outlook.com>
2024-09-02 18:53:10 -0300

    :sparkles: basic http requests

--- src/main/java/com/scheffel/api_livraria/controllers/BookController.java
+++ src/main/java/com/scheffel/api_livraria/controllers/BookController.java
@@ -1,4 +1,38 @@
 package com.scheffel.api_livraria.controllers;
 
+import com.scheffel.api_livraria.entities.Book;
+import com.scheffel.api_livraria.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/book")
 public class BookController {
+
+    @Autowired
+    private BookService service;
+
+    @GetMapping("/findAll")
+    public ResponseEntity<List<Book>> findAll() {
+        try {
+            List<Book> books = service.findAll();
+            return new ResponseEntity<>(books, HttpStatus.OK);
+        } catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody Book book) {
+        try {
+            this.service.save(book);
+            return new ResponseEntity<>("Book registered successfully" ,HttpStatus.OK);
+        } catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- src/main/java/com/scheffel/api_livraria/services/BookService.java
+++ src/main/java/com/scheffel/api_livraria/services/BookService.java
@@ -1,4 +1,23 @@
 package com.scheffel.api_livraria.services;
 
+import com.scheffel.api_livraria.entities.Book;
+import com.scheffel.api_livraria.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookService {
+
+    @Autowired
+    private BookRepository repository;
+
+    public List<Book> findAll() {
+        return this.repository.findAll();
+    }
+
+    public void save(Book book) {
+        this.repository.save(book);
+    }
 }
